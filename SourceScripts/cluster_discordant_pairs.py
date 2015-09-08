d2e0194d67d4700bbba88e7f86699f58c5743329
Austin <rossa38@unlv.nevada.edu>
2015-09-08 13:55:16 -0700

    Added buffer range (ismean) to clustering discordant reads, removed strand dependency, and removed minimum discordant reads requirement while clustering (still used when searching for breakpoints)

--- SourceScripts/cluster_discordant_pairs.py
+++ SourceScripts/cluster_discordant_pairs.py
@@ -7,141 +7,77 @@ import itertools
 def main(args):
   basename = os.path.splitext(os.path.basename(args[1]))[0]
   discfile = pysam.Samfile(args[1], "rb")
-  minclustsize = int(args[2])
-  readdepth = int(args[3])
-  isfile = open(args[4], 'r')
+  readdepth = int(args[2])
+  isfile = open(args[3], 'r')
   ismean = float(isfile.readline().strip('\n'))
   isfile.close()
 
   ## Magic Numbers
   minqual = 5 # minimum quality to filter out multi-mapped reads
   readlen = 100 # read length
+  bufrange = int(ismean) # buffer range between discordant reads
   # This is to prevent large repeat regions from clogging file writing pipeline
-  maxclustsize = (readdepth*ismean)/readlen
+  #maxclustsize = (readdepth*ismean*15)/readlen
 
   rangesfile = open("Results/" + basename + ".clusters.ranges.txt", 'w')
-  rangesfile.write("chromosome\tcluster\tstart\tend\tnum_of_reads\tstrand\n")
+  rangesfile.write("chromosome\tcluster\tstart\tend\tnum_of_reads\n")
   clusfile = open("Results/" + basename + ".clusters.txt", 'w')
 
-  poscluster = {}
-  negcluster = {}
-  posclusternum = 0
-  negclusternum = 0
-  negtot = 0
-  postot = 0
+  cluster = {}
+  clusternum = 0
   totcluster = 0
   chrom = 0
   bam_iter = discfile.__iter__()
 
+  def writeCluster():
+    #if (clusternum >= minclustsize): #and clusternum <= maxclustsize):
+    clusfile.write(">%i\n" % (totcluster))
+
+    for value in cluster.itervalues():
+      clusfile.write("%s\\%i\n" % (value.qname, value.is_read1))
+
+    rangesfile.write("%s\t%i\t%i\t%i\t%i\n" % (discfile.getrname(chrom), totcluster, leftpos+1, rightpos+1, clusternum))
+    return True
+    #return False
+
+
   for read in bam_iter:
     if (read.mapq > 5): # Do not cluster multi-mapping reads
+      read_pos = read.pos
+      if (read.cigar[0][0] == 4):
+        read_pos -= read.cigar[0][1]
       if (read.tid == chrom):
-        if (read.is_reverse):
-          if (negclusternum == 0):
-            negleftpos = read.pos
-            negrightpos = read.pos + read.rlen
-            negcluster[negclusternum] = read
-            negclusternum += 1
-          elif (read.pos > negleftpos and read.pos <= negrightpos):
-            negcluster[negclusternum] = read
-            if (read.cigar[-1][0] != 4):
-              negrightpos = read.pos + read.rlen
-            else:
-              negrightpos = read.pos + read.rlen - read.cigar[-1][1]
-            negclusternum += 1
-          else:
-            if (negclusternum >= minclustsize and negclusternum <= maxclustsize):
-              if (negtot == postot):
-                totcluster += 1
-                negtot = totcluster
-              clusfile.write(">%i\n" % (negtot))
-
-              for value in negcluster.itervalues():
-                clusfile.write("%s\\%i\n" % (value.qname, value.is_read1))
-
-              rangesfile.write("%s\t%i\t%i\t%i\t%i\t%s\n" % (discfile.getrname(chrom), negtot, negleftpos+1, negrightpos+1, negclusternum, '-'))
-              totcluster += 1
-              negtot = totcluster
-
-            negclusternum = 0
-            negcluster.clear()
-            negcluster[0] = read
-            negclusternum += 1
-            negleftpos = read.pos
-            negrightpos = read.pos + read.rlen
-
+        if (clusternum == 0):
+          leftpos = read_pos - bufrange
+          rightpos = read_pos + read.rlen + bufrange
+          cluster[clusternum] = read
+          clusternum += 1
+        elif (read_pos > leftpos and read_pos <= rightpos):
+          cluster[clusternum] = read
+          rightpos = read_pos + read.rlen + bufrange
+          clusternum += 1
         else:
-          if (posclusternum == 0):
-            posleftpos = read.pos
-            posrightpos = read.pos + read.rlen
-            poscluster[posclusternum] = read
-            posclusternum += 1
-          elif (read.pos > posleftpos and read.pos <= posrightpos):
-            poscluster[posclusternum] = read
-            if (read.cigar[-1][0] != 4):
-              posrightpos = read.pos + read.rlen
-            else:
-              posrightpos = read.pos + read.rlen - read.cigar[-1][1]
-            posclusternum += 1
-          else:
-            if (posclusternum >= minclustsize and posclusternum <= maxclustsize):
-              if (negtot == postot):
-                totcluster += 1
-                postot = totcluster
-              clusfile.write(">%i\n" % (postot))
-
-              for value in poscluster.itervalues():
-                clusfile.write("%s\\%i\n" % (value.qname, value.is_read1))
-
-              rangesfile.write("%s\t%i\t%i\t%i\t%i\t%s\n" % (discfile.getrname(chrom), postot, posleftpos+1, posrightpos+1, posclusternum, '+'))
-              totcluster += 1
-              postot = totcluster
-
-            posclusternum = 0
-            poscluster.clear()
-            poscluster[0] = read
-            posclusternum += 1
-            posleftpos = read.pos
-            posrightpos = read.pos + read.rlen
+          if (writeCluster()):
+            totcluster += 1
+          clusternum = 0
+          cluster.clear()
+          cluster[0] = read
+          clusternum += 1
+          leftpos = read_pos - bufrange
+          rightpos = read_pos + read.rlen + bufrange
 
       else:
-        if (negclusternum >= minclustsize and negclusternum <= maxclustsize):
-          clusfile.write(">%i\n" % (negtot))
-
-          for value in negcluster.itervalues():
-            clusfile.write("%s\\%i\n" % (value.qname, value.is_read1))
-
-          rangesfile.write("%s\t%i\t%i\t%i\t%i\t%s\n" % (discfile.getrname(chrom), negtot, negleftpos+1, negrightpos+1, negclusternum+1, '-'))
+        if (writeCluster()):
           totcluster += 1
-          negtot = totcluster
+        clusternum = 0
+        cluster.clear()
+        cluster[0] = read
+        clusternum += 1
+        leftpos = read_pos - bufrange
+        rightpos = read_pos + read.rlen + bufrange
+        chrom = read.tid
 
-        negclusternum = 0
-        negcluster.clear()
-
-        if (read.is_reverse):
-          negcluster[0] = read
-          chrom = read.tid
-          negleftpos = read.pos
-          negrightpos = read.pos + read.rlen
-
-        if (posclusternum >= minclustsize and posclusternum <= maxclustsize):
-          clusfile.write(">%i\n" % (postot))
-
-          for value in poscluster.itervalues():
-            clusfile.write("%s\\%i\n" % (value.qname, value.is_read1))
-
-          rangesfile.write("%s\t%i\t%i\t%i\t%i\t%s\n" % (discfile.getrname(chrom), postot, posleftpos+1, posrightpos+1, posclusternum+1, '+'))
-          totcluster += 1
-          postot = totcluster
-
-        posclusternum = 0
-        poscluster.clear()
-
-        if (not read.is_reverse):
-          poscluster[0] = read
-          chrom = read.tid
-          posleftpos = read.pos
-          posrightpos = read.pos + read.rlen
+  writeCluster()
 
   discfile.close()
   rangesfile.close()
